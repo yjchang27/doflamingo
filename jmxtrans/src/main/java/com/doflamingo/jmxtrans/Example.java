981cbed3b9a3de8bf4b5d4870d53c65e84dbfe75
Snyo <blueonion24@gmail.com>
2016-05-25 04:43:09 +0900

    RRD Writer first attempt

--- jmxtrans/src/main/java/com/doflamingo/jmxtrans/Example.java
+++ jmxtrans/src/main/java/com/doflamingo/jmxtrans/Example.java
@@ -6,8 +6,14 @@ import com.googlecode.jmxtrans.cli.JmxTransConfiguration;
 import com.googlecode.jmxtrans.guice.JmxTransModule;
 import com.googlecode.jmxtrans.model.JmxProcess;
 import com.googlecode.jmxtrans.util.JsonUtils;
+import org.rrd4j.ConsolFun;
+import org.rrd4j.core.FetchRequest;
+import org.rrd4j.core.RrdDb;
+import org.rrd4j.core.Util;
+import org.rrd4j.core.FetchData;
 
 import java.io.File;
+import java.util.Scanner;
 
 /**
  * Created by yjchang on 5/11/16.
@@ -22,12 +28,29 @@ public class Example {
         JsonUtils jsonUtils = injector.getInstance(JsonUtils.class);
 
         JmxProcess process = jsonUtils.parseProcess(
-                new File(Example.class.getClassLoader().getResource("example.json").getFile())
+                new File(Example.class.getClassLoader().getResource("exampleConfig.json").getFile())
         );
 
         JmxTransformer transformer = injector.getInstance(JmxTransformer.class);
         transformer.executeStandalone(process);
 
+        long endTime = Util.getTime();
+        long startTime = endTime - (12*60*60L);
+
+        Scanner sc = new Scanner(System.in);
+        String newString;
+        while(true) {
+            newString = sc.nextLine();
+            System.out.println(newString);
+
+            String rrdPath = Example.class.getClassLoader().getResource("example.rrd").getFile();
+            System.out.println(rrdPath);
+            RrdDb rrdDb = new RrdDb(rrdPath, true);
+
+            FetchRequest fetchRequest = rrdDb.createFetchRequest(ConsolFun.MAX, startTime, endTime);
+            FetchData fetchData = fetchRequest.fetchData();
+            System.out.println(fetchData.dump());
+        }
     }
 
 }
---
+++ jmxtrans/src/main/java/com/doflamingo/jmxtrans/RRDWriter.java
@@ -0,0 +1,143 @@
+/**
+ * The MIT License
+ * Copyright (c) 2010 JmxTrans team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.doflamingo.jmxtrans;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableList;
+import com.googlecode.jmxtrans.model.Query;
+import com.googlecode.jmxtrans.model.Result;
+import com.googlecode.jmxtrans.model.Server;
+import com.googlecode.jmxtrans.model.ValidationException;
+import com.googlecode.jmxtrans.model.output.BaseOutputWriter;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.jexl2.UnifiedJEXL;
+import org.rrd4j.core.RrdDb;
+import org.rrd4j.core.RrdDef;
+import org.rrd4j.core.RrdDefTemplate;
+import org.rrd4j.core.Sample;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.googlecode.jmxtrans.util.NumberUtils.isNumeric;
+
+/**
+ * This takes a JRobin template.xml file and then creates the database if it
+ * doesn't already exist.
+ *
+ * It will then write the contents of the Query (the Results) to the database.
+ *
+ * This uses the JRobin rrd format and is incompatible with the C version of
+ * rrd.
+ *
+ * @author jon
+ */
+public class RRDWriter extends BaseOutputWriter {
+
+    private final File outputFile;
+    private final File templateFile;
+
+    @JsonCreator
+    public RRDWriter(
+            @JsonProperty("typeNames") ImmutableList<String> typeNames,
+            @JsonProperty("booleanAsNumber") boolean booleanAsNumber,
+            @JsonProperty("debug") Boolean debugEnabled,
+            @JsonProperty("outputFile") String outputFile,
+            @JsonProperty("templateFile") String templateFile,
+            @JsonProperty("settings") Map<String, Object> settings) {
+        super(typeNames, booleanAsNumber, debugEnabled, settings);
+
+//        this.outputFile = new File(MoreObjects.firstNonNull(outputFile, (String) getSettings().get(OUTPUT_FILE)));
+//        this.templateFile = new File(MoreObjects.firstNonNull(templateFile, (String) getSettings().get(TEMPLATE_FILE)));
+
+        if(Example.class.getClassLoader().getResource(MoreObjects.firstNonNull(outputFile, (String) getSettings().get(OUTPUT_FILE))) != null) {
+            this.outputFile = new File(Example.class.getClassLoader().getResource(MoreObjects.firstNonNull(outputFile, (String) getSettings().get(OUTPUT_FILE))).getFile());
+        } else {
+            this.outputFile = new File("");
+        }
+        this.templateFile = new File(Example.class.getClassLoader().getResource(MoreObjects.firstNonNull(outputFile, (String) getSettings().get(TEMPLATE_FILE))).getFile());
+//        checkState(this.outputFile.exists(), "Output file must exist");
+//        checkState(this.templateFile.exists(), "Template file must exist");
+    }
+
+    @Override
+    public void validateSetup(Server server, Query query) throws ValidationException {}
+
+    @Override
+    public void internalWrite(Server server, Query query, ImmutableList<Result> results) throws Exception {
+        RrdDb db = null;
+        try {
+            db = createOrOpenDatabase();
+            Sample sample = db.createSample();
+            List<String> dsNames = Arrays.asList(db.getDsNames());
+
+            // go over all the results and look for datasource names that map to
+            // keys from the result values
+            for (Result res : results) {
+                for (Entry<String, Object> entry : res.getValues().entrySet()) {
+                    if (dsNames.contains(entry.getKey()) && isNumeric(entry.getValue())) {
+                        sample.setValue(entry.getKey(), Double.valueOf(entry.getValue().toString()));
+                    }
+                }
+            }
+            sample.update();
+        } finally {
+            if (db != null) {
+                db.close();
+            }
+        }
+    }
+
+    /**
+     * If the database file doesn't exist, it'll get created, otherwise, it'll
+     * be returned in r/w mode.
+     */
+    protected RrdDb createOrOpenDatabase() throws Exception {
+        RrdDb result;
+        if (!this.outputFile.exists()) {
+            System.out.println("Create New DB");
+            FileUtils.forceMkdir(this.outputFile.getParentFile());
+            RrdDefTemplate t = new RrdDefTemplate(this.templateFile);
+            t.setVariable("database", this.outputFile.getCanonicalPath());
+            RrdDef def = t.getRrdDef();
+            result = new RrdDb(def);
+        } else {
+            result = new RrdDb(this.outputFile.getCanonicalPath());
+        }
+        return result;
+    }
+
+    public String getTemplateFile() {
+        return templateFile.getPath();
+    }
+
+    public String getOutputFile() {
+        return outputFile.getPath();
+    }
+}
